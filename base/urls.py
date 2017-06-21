5f668d642d7b016e93886b64390147426bebb7e4
Diego Perez Rebolledo <diego@Air-de-Diego.lan>
2017-06-21 16:33:37 -0400

    urls base

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from base import views
 
 urlpatterns = [
-    url(r'^index/$', views.index_view, name='index'),
+    url(r'^$', views.index_view, name='index'),
     url(r'^detalle/(?P<id>\d+)$', views.detalle_view, name='detalle'),
 ]
