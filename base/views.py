5aa64b9c09afdb8dfe90c9bf0105fd64756f5011
Diego Perez Rebolledo <diego@MacBook-Air-de-Diego.local>
2017-06-21 02:10:16 -0400

    imagenes funcionando

--- base/views.py
+++ base/views.py
@@ -10,12 +10,12 @@ def index_view(request):
     noticias            = Noticia.objects.filter(destacada = False).order_by('-fecha_publicacion')[:6]
     return render(request, 'index.html', {
         'noticias_destacada' : noticias_destacada,
-        'noticias'           : noticias
+        'noticias'           : noticias,
     })
 
 
 def detalle_view(request, id):
     noticia = get_object_or_404(Noticia, id = id)
     return render(request, 'detalle.html', {
-        'noticia' : noticia
+        'noticia' : noticia,
     })
